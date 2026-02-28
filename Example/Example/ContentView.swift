0247305350b951a500108b4ae6612ab67c1bd28a
Emilio Peláez <me@epelaez.net>
2026-02-28 14:40:12 +0100

    Create Example Project

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Emilio Peláez on 28/2/26.
+//
+
+import SwiftUI
+import SymbolPicker
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Emilio Peláez on 28/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
